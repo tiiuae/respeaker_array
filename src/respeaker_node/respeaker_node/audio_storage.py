87400712234126158e0a2916666cef50ceabf289
Kide Vuojärvi <kide.vuojarvi@gmail.com>
2021-07-05 16:51:50 +0300

    Rework audio storage node
    * Audio storage now creates a timestamped directory when topic is
      written into
    * Raw audio is written into a file once per second
    * When no new data is published, node combines the short audio
      files into one file per channel, then erases temporary files.
    * New recording is started into a new directory when new data is
      published.

--- src/respeaker_node/respeaker_node/audio_storage.py
+++ src/respeaker_node/respeaker_node/audio_storage.py
@@ -1,6 +1,8 @@
 import pyaudio
 import numpy as np
 import wave
+from time import time
+from os import mkdir, rmdir, remove, listdir
 
 import rclpy
 from rclpy.node import Node
@@ -22,21 +24,30 @@ class AudioStorageNode(Node):
         self._pa = pyaudio.PyAudio()
 
         self._count = 0
+        self._path = None
 
         # QoS_policy should match the one defined in the publisher
         self.subscription = self.create_subscription(AudioBuffer, "RawAudio_PubSubTopic", self.callback,
                                                      rclpy.qos.qos_profile_sensor_data)
-        self._outfile = "test"
 
-        self._timer = self.create_timer(10, self.store_audio)
+        self._outfile = "audio_{:d}/ch{:d}/{:d}.wav"
+
+        self._timer = self.create_timer(1, self.store_audio)
 
     def store_audio(self):
         if not any(self._frames):
-            self.get_logger().info("No frames received: files not written")
+            if not self._count:
+                self.get_logger().info("No frames received: files not written")
+            else:
+                self.get_logger().info("Publisher has stopped publishing, combining files.")
+                self.combine()
+                self._count = 0
+                self._path = None
             return
+
         if self._stored_channels & 0b100:
             self.get_logger().info("Writing processed audio into {:s}{:d}.wav".format(self._outfile, self._count))
-            wf = wave.open("{:s}{:d}.wav".format(self._outfile, self._count), 'wb')
+            wf = wave.open(self._outfile.format(self._path, 0, self._count), 'wb')
             wf.setnchannels(1)
             wf.setsampwidth(self._pa.get_sample_size(self._pa.get_format_from_width(SAMPLE_WIDTH)))
             wf.setframerate(SAMPLE_RATE)
@@ -47,7 +58,7 @@ class AudioStorageNode(Node):
             self.get_logger().info("Writing raw audio files for each microphone")
             for j in range(4):
                 self.get_logger().info("Writing raw audio into {:s}{:d}.wav".format(self._outfile, self._count))
-                wf = wave.open("{:s}_rawch{:d}_{:d}.wav".format(self._outfile, j, self._count), 'wb')
+                wf = wave.open(self._outfile.format(self._path, j + 1, self._count), 'wb')
                 wf.setnchannels(1)
                 wf.setsampwidth(self._pa.get_sample_size(self._pa.get_format_from_width(SAMPLE_WIDTH)))
                 wf.setframerate(SAMPLE_RATE)
@@ -56,7 +67,7 @@ class AudioStorageNode(Node):
 
         if self._stored_channels & 0b1:
             self.get_logger().info("Writing background audio into {:s}{:d}.wav".format(self._outfile, self._count))
-            wf = wave.open("{:s}_bg_{:d}.wav".format(self._outfile, self._count), 'wb')
+            wf = wave.open(self._outfile.format(self._path, 5, self._count), 'wb')
             wf.setnchannels(1)
             wf.setsampwidth(self._pa.get_sample_size(self._pa.get_format_from_width(SAMPLE_WIDTH)))
             wf.setframerate(SAMPLE_RATE)
@@ -66,9 +77,45 @@ class AudioStorageNode(Node):
         self._count += 1
 
     def callback(self, msg):
+        if self._path is None:
+            self._path = int(time())
+            print(self._path)
+            self.create_dirs()
         a = msg.data
         for j in range(5):
             self._frames[j].append(a[j::6].tostring())
+
+    def create_dirs(self):
+        base_dir = "audio_{:d}".format(self._path)
+        mkdir(base_dir)
+        for i in range(6):
+            mkdir(base_dir + "/ch{:d}".format(i))
+
+    def combine(self):
+        for j in range(6):
+            combine_files("audio_{:d}/".format(self._path), "ch{:d}".format(j))
+
+
+def combine_files(path, subdir):
+    source_dir = path + subdir + "/"
+    infiles = listdir(source_dir)
+    outfile = path + subdir + ".wav"
+
+    if infiles:
+        data = []
+        for infile in infiles:
+            w = wave.open(source_dir + infile, 'rb')
+            data.append([w.getparams(), w.readframes(w.getnframes())])
+            w.close()
+            remove(source_dir + infile)
+
+        output = wave.open(outfile, 'wb')
+        output.setparams(data[0][0])
+        for i in range(len(data)):
+            output.writeframes(data[i][1])
+        output.close()
+
+    rmdir(source_dir)
 
 
 def main(args=None):
